01ea61f893284a78485c134ad80f26072bf2db3a
Adam Dunai <dunai.adam95@gmail.com>
2022-02-17 17:12:30 +0100

    Set up dependency injection

--- app/src/main/java/com/example/mapvisualisation/MapVisualisationApplication.kt
+++ app/src/main/java/com/example/mapvisualisation/MapVisualisationApplication.kt
@@ -1,5 +1,7 @@
 package com.example.mapvisualisation
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MapVisualisationApplication : Application()
--- app/src/main/java/com/example/mapvisualisation/main/MainActivity.kt
+++ app/src/main/java/com/example/mapvisualisation/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.example.mapvisualisation
+package com.example.mapvisualisation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mapvisualisation.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
