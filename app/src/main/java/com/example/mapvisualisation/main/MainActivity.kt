2e40f63065b291f1af257f212ce3791681874bae
Adam Dunai <dunai.adam95@gmail.com>
2022-02-17 17:17:28 +0100

    Enable view binding

--- app/src/main/java/com/example/mapvisualisation/main/MainActivity.kt
+++ app/src/main/java/com/example/mapvisualisation/main/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.mapvisualisation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mapvisualisation.R
+import com.example.mapvisualisation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityBinding.root)
     }
 }
