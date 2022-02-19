0569edf7334cbccbb6978f53439e2c41c04ca3ac
Adam Dunai <dunai.adam95@gmail.com>
2022-02-19 17:25:36 +0100

    Check location permission and move camera to current position

--- app/src/main/java/com/example/mapvisualisation/main/MainActivity.kt
+++ app/src/main/java/com/example/mapvisualisation/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mapvisualisation.main
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -10,19 +12,34 @@ import com.example.mapvisualisation.R
 import com.example.mapvisualisation.databinding.ActivityMainBinding
 import com.example.mapvisualisation.main.model.ScooterClusterItem
 import com.example.mapvisualisation.main.viewmodel.ScooterMapViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.material.snackbar.Snackbar
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.ktx.awaitMap
 import com.google.maps.android.ktx.awaitMapLoad
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnNeverAskAgain
+import permissions.dispatcher.OnPermissionDenied
+import permissions.dispatcher.RuntimePermissions
 
 @AndroidEntryPoint
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
 
     private lateinit var activityBinding: ActivityMainBinding
     private lateinit var clusterManager: ClusterManager<ScooterClusterItem>
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var cancellationToken: CancellationTokenSource
 
     private val viewModel: ScooterMapViewModel by viewModels()
 
@@ -31,6 +48,8 @@ class MainActivity : AppCompatActivity() {
 
         activityBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityBinding.root)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         initMap()
     }
@@ -51,6 +70,8 @@ class MainActivity : AppCompatActivity() {
                     googleMap.setOnCameraIdleListener(clusterManager)
                 }
 
+                moveCameraToCurrentLocationWithPermissionCheck(googleMap)
+
                 viewModel.scooterListFlow.collectLatest { clusterList ->
                     addClusters(clusterList)
                 }
@@ -64,5 +85,64 @@ class MainActivity : AppCompatActivity() {
             addItems(clusterList)
             cluster()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        cancellationToken = CancellationTokenSource()
+    }
+
+    @SuppressLint("MissingPermission")
+    @NeedsPermission(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    fun moveCameraToCurrentLocation(map: GoogleMap) {
+        fusedLocationClient.getCurrentLocation(
+            LocationRequest.PRIORITY_HIGH_ACCURACY,
+            cancellationToken.token
+        ).addOnSuccessListener { location ->
+            map.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    LatLng(location.latitude, location.longitude),
+                    MAP_CAMERA_ZOOM_LEVEL
+                )
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+    @OnPermissionDenied(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    @OnNeverAskAgain(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    fun onLocationPermissionDenied() {
+        Snackbar.make(
+            activityBinding.root,
+            R.string.scooter_map_location_permission_denied,
+            Snackbar.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        cancellationToken.cancel()
+    }
+
+    companion object {
+        const val MAP_CAMERA_ZOOM_LEVEL = 10f
     }
 }
